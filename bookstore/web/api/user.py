939cc493791aa5a9445fe88650de19674fd04b7d
huong-nt <nguyentienhuong93@gmail.com>
2022-01-28 04:39:15 +0900

    return xml or json for user api

--- bookstore/web/api/user.py
+++ bookstore/web/api/user.py
@@ -1,7 +1,6 @@
 
 import uuid
-from xml.etree.ElementInclude import FatalIncludeError
-from flask import jsonify
+from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 import werkzeug
@@ -12,6 +11,7 @@ from .. import db, boto
 from ..utils import Utils
 from ..utils.s3 import S3
 from ..models import User, Book, Permission
+from ..utils.response import build_response, get_content_type
 
 utils = Utils()
 
@@ -27,26 +27,28 @@ class UserView(Resource):
 
     @jwt_required()
     def get(self):
+        content_type = get_content_type(request.args)
         # get basic information of user in JWT token
         current_user = get_jwt_identity()
         # find user by username
         user = User.query.filter_by(id=current_user['userid']).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
 
         # return user information
-        return jsonify({
+        return build_response({
             'ok': True,
             'code': 200,
             'data': user.get_response()
-        })
+        }, content_type)
 
     @jwt_required()
     def put(self):
+        content_type = get_content_type(request.args)
         # parse params
         args = self.parser.parse_args()
         params = {
@@ -64,11 +66,11 @@ class UserView(Resource):
         # find user by user_id
         user = User.query.filter_by(id=current_user['userid']).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
 
         # update user with request params
         for key, value in params.items():
@@ -77,11 +79,11 @@ class UserView(Resource):
         try:
             db.session.commit()
             # return user information
-            return jsonify({
+            return build_response({
                 'ok': True,
                 'code': 200,
                 'data': user.get_response()
-            })
+            }, content_type)
         except Exception as e:
             # Rollback if it have any error
             logger.error(e)
@@ -91,38 +93,38 @@ class UserView(Resource):
                 'message': 'internal server error'
             }, 200
         
-
     @jwt_required()
     def delete(self):
+        content_type = get_content_type(request.args)
         # get basic information of user in JWT token
         current_user = get_jwt_identity()
         # find user by user_id
         user = User.query.filter_by(id=current_user['userid']).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
 
         try:
             # Detele all published book and then, delete the user
             Book.query.filter_by(author_id=user.id).delete()
             db.session.delete(user)
             db.session.commit()
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 200,
-            })
+            }, content_type)
         except Exception as e:
             # Rollback if it have any error
             logger.error(e)
             db.session.rollback()
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 500,
                 'message': 'internal server error'
-            })
+            }, content_type)
 
 
 api_restful.add_resource(UserView, '/users')
@@ -132,8 +134,7 @@ class UserPublicBookView(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('title', help='title cannot be blank')  # default type: unicode
     parser.add_argument('description')  # default type: unicode
-    parser.add_argument(
-        'cover', type=werkzeug.datastructures.FileStorage, location='files')
+    parser.add_argument('cover', type=werkzeug.datastructures.FileStorage, location='files')
     parser.add_argument('price', type=int)
 
     def __init__(self) -> None:
@@ -145,42 +146,44 @@ class UserPublicBookView(Resource):
 
     @jwt_required()
     def get(self, user_id):
+        content_type = get_content_type(request.args)
         # get basic information of user in JWT token
         current_user = get_jwt_identity()
         if user_id != current_user['userid']:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 403,
                 'message': 'jwt token not belong to user_id'
-            })
+            }, content_type)
         # find user by id
         user = User.query.filter_by(id=user_id).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
 
         books = Book.query.filter_by(author_id=user_id).all()
         res = [book.to_json() for book in books]
 
-        return jsonify({
+        return build_response({
             'ok': True,
             'code': 200,
             'data': res
-        })
+        }, content_type)
 
     @jwt_required()
     def post(self, user_id):
+        content_type = get_content_type(request.args)
         # get basic information of user in JWT token
         current_user = get_jwt_identity()
         if user_id != current_user['userid']:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 403,
                 'message': 'jwt token not belong to user_id'
-            })
+            }, content_type)
 
         args = self.parser.parse_args()
         logger.info(args)
@@ -188,17 +191,17 @@ class UserPublicBookView(Resource):
         # find user by id
         user = User.query.filter_by(id=user_id).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
         if user.can(Permission.PUBLISH) == False:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 401,
                 'message': 'user does not have permission to publish the book'
-            })
+            }, content_type)
         # parse book cover file content
         book_cover_url = None
         if 'cover' in args and args['cover'] != None and args['cover'].filename != '':
@@ -206,11 +209,11 @@ class UserPublicBookView(Resource):
             file_name = secure_filename(file_cover.filename)
             file_extension = file_name.rsplit('.', 1)[1].lower()
             if file_extension not in ALLOWED_EXTENSIONS:
-                return jsonify({
+                return build_response({
                     'ok': False,
                     'code': 400,
                     'message': 'file extension is not one of our supported types'
-                })
+                }, content_type)
             # TODO: upload book cover to S3 (set public access)
             image_key_name = f'books/cover/' + uuid.uuid4().hex + '.' + file_extension
             book_cover_url = self.s3.upload_public(self.S3_BUCKET_NAME, image_key_name, file_cover, file_extension)
@@ -225,30 +228,31 @@ class UserPublicBookView(Resource):
             )
             db.session.add(book)
             db.session.commit()
-            return jsonify({
+            return build_response({
                 'ok': True,
                 'code': 200,
                 'data': book.get_response()
-            })
+            }, content_type)
         except Exception as e:
             logger.error(e)
             db.session.rollback()
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 500,
                 'message': 'internal server error'
-            })
+            }, content_type)
 
     @jwt_required()
     def put(self, user_id, book_id):
+        content_type = get_content_type(request.args)
         # get basic information of user in JWT token
         current_user = get_jwt_identity()
         if user_id != current_user['userid']:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 403,
                 'message': 'jwt token not belong to user_id'
-            })
+            }, content_type)
 
         args = self.parser.parse_args()
         logger.info(args)
@@ -256,18 +260,18 @@ class UserPublicBookView(Resource):
         # find user by id
         user = User.query.filter_by(id=user_id).first()
         if user is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
             })
         book = Book.query.filter_by(id=book_id).first()
         if book is None:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 404,
                 'message': 'user not found'
-            })
+            }, content_type)
         
         params = {
             'title': args['title'],
@@ -282,11 +286,11 @@ class UserPublicBookView(Resource):
             file_extension = file_name.rsplit('.', 1)[1].lower()
             # Check file extension
             if file_extension not in ALLOWED_EXTENSIONS:
-                return jsonify({
+                return build_response({
                     'ok': False,
                     'code': 400,
                     'message': 'file extension is not one of our supported types'
-                })
+                }, content_type)
             # upload book cover to S3 (set public access)
             image_key_name = f'books/cover/' + uuid.uuid4().hex + '.' + file_extension
             params['cover'] = self.s3.upload_public(self.S3_BUCKET_NAME, image_key_name, file_cover, file_extension)
@@ -299,15 +303,15 @@ class UserPublicBookView(Resource):
             # commit the changes to database
             db.session.commit()
             # return user information
-            return jsonify({
+            return build_response({
                 'ok': True,
                 'code': 200,
                 'data': book.get_response()
-            })
+            }, content_type)
         except Exception as e:
             logger.error(e)
             db.session.rollback()
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 500,
                 'message': 'internal server error'
@@ -315,31 +319,33 @@ class UserPublicBookView(Resource):
     
     @jwt_required()
     def delete(self, user_id, book_id):
+        content_type = get_content_type(request.args)
+        # get basic information of user in JWT token
         current_user = get_jwt_identity()
         if user_id != current_user['userid']:
-            return jsonify({
+            return build_response({
                 'ok': False,
                 'code': 403,
                 'message': 'jwt token not belong to user_id'
-            })
+            }, content_type)
         
         try:
             # Delete a published book
             Book.query.filter_by(id=book_id, author_id=user_id).delete()
             db.session.commit()
-            return jsonify({
+            return build_response({
                 'ok': True,
                 'code': 200,
-            })
+            }, content_type)
         except Exception as e:
             # Rollback if it have any error
             logger.error(e)
             db.session.rollback()
-            return {
+            return build_response({
                 'ok': False,
                 'code': 500,
                 'message': 'internal server error'
-            }, 200
+            }, content_type)
 
 
 api_restful.add_resource(
