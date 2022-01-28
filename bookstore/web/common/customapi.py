792f623f08b0718ce43ec7529c1b0f961d139b8e
huong-nt <nguyentienhuong93@gmail.com>
2022-01-28 12:33:21 +0900

    return xml or json for error handler

--- bookstore/web/common/customapi.py
+++ bookstore/web/common/customapi.py
@@ -1,19 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import request
 import flask_restful
 import logging
 from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
 from flask_jwt_extended.exceptions import NoAuthorizationError, JWTDecodeError, InvalidHeaderError
 
+from ..utils.response import build_response, get_content_type
+
 class CustomApi(flask_restful.Api):
     def handle_error(self, e):
+        content_type = get_content_type(request.args)
         if isinstance(e, ExpiredSignatureError):
-            return jsonify({"ok": False, "error": "token has expired"}), 401
+            return build_response({"ok": False, "code": 401, "error": "token has expired"}, content_type)
         if isinstance(e, NoAuthorizationError):
-            return jsonify({"ok": False, "error": "token is missing"}), 401
+            return build_response({"ok": False, "code": 401, "error": "token is missing"}, content_type)
         if isinstance(e, InvalidHeaderError):
-            return jsonify({"ok": False, "error": "token is missing"}), 401
+            return build_response({"ok": False, "code": 401, "error": "token is missing"}, content_type)
         if isinstance(e, JWTDecodeError) or isinstance(e, InvalidSignatureError):
-            return jsonify({"ok": False, "error": "can not decode the token"}), 403
+            return build_response({"ok": False, "code": 403, "error": "can not decode the token"}, content_type)
 
         message = ''
         if hasattr(e, 'message'):
@@ -23,11 +26,13 @@ class CustomApi(flask_restful.Api):
                 message = str(e.data['message'])
         if hasattr(e, 'description') and message == '':
             message = e.description
-            
+        
         response = {"ok": False, 'error': message}
-        try:
-            return jsonify(response), e.code
-        except Exception as e:
+        if hasattr(e, 'code'):
+            response['code'] = e.code
+            return build_response(response, content_type)
+        else:
+            response['code'] = 500
             for attr in dir(e):
                 logging.error("obj.%s = %r" % (attr, getattr(e, attr)))
-            return jsonify(response), 500
+            return build_response(response, content_type)
